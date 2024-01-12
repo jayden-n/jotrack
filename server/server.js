7956503b0b2fdf80899dab4ff4b00fd7096c6900
Jayden Nguyen <jaydennguyen.dev@gmail.com>
2024-01-12 18:02:26 -0500

    refactor: create server structure; add in 'not found' middleware

--- server/server.js
+++ server/server.js
@@ -6,6 +6,11 @@ app.get("/", (req, res) => {
 	res.send("hello world");
 });
 
+//  ================== "NOT FOUND" MIDDLEWARE  ==================
+app.use("*", (req, res) => {
+	res.status(404).json({ msg: "not found" });
+});
+
 app.listen(8000, () => {
 	console.log("server is running!!!....");
 });
