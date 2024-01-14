b3141ebef030a48af9727e1fac9d72803671ae8d
Jayden Nguyen <jaydennguyen.dev@gmail.com>
2024-01-14 00:05:28 -0500

    feat(server): initialized job router with server base url

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,20 +2,26 @@ import express from "express";
 import { Request, Response, Application } from "express";
 import dotenv from "dotenv";
 
+// ================== ROUTER IMPORTS ==================
+import jobRouter from "../routes/jobRouter.js";
+
 const serverPort: number = 8000;
 dotenv.config();
 
 const app: Application = express();
 
 app.get("/", (request: Request, response: Response): void => {
-  response.send("hello world");
+	response.send("hello world");
 });
+
+// ================== BASE URL ==================
+app.use("api/v1/jobs", jobRouter);
 
 //  ================== "NOT FOUND" MIDDLEWARE  ==================
 app.use("*", (request: Request, response: Response): void => {
-  response.status(404).json({ msg: "not found" });
+	response.status(404).json({ msg: "not found" });
 });
 
 app.listen(serverPort, () => {
-  console.log(`server is running at port ${serverPort}`);
+	console.log(`server is running at port ${serverPort}`);
 });
