60b3b73ba2816da80c7d34d9102edd11c6bca5f4
sarahrami <sarahrami97@gmail.com>
2024-01-18 00:15:13 -0500

    update and add job pages added

---
+++ client/src/pages/admin/AddJob.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import NavBar from './NavBar';
+export default function AddJob() {
+  return (
+    <div>
+        <NavBar/>
+            <div>AddJob</div>
+    </div>
+
+  )
+}
