b54669fd51df5c4d5fece0bbfac8a35a998f3305
Jayden Nguyen <jaydennguyen.dev@gmail.com>
2024-01-19 15:27:03 -0500

    feat: complete job CRUD operations through router

--- server/controllers/jobController.ts
+++ server/controllers/jobController.ts
@@ -1,6 +1,6 @@
+import { StatusCodes } from "http-status-codes";
 import { Request, Response } from "express";
 import Job from "../models/JobModel.js";
-import { StatusCodes } from "http-status-codes";
 import { IJobs } from "../interface/IJob.js";
 
 // ================== GET ALL JOBS ==================
@@ -19,3 +19,28 @@ export const createJob = async (req: Request, res: Response): Promise<void> => {
 
 	res.status(StatusCodes.CREATED).json({ job });
 };
+
+// ================== GET SINGLE JOB ==================
+export const getSingleJob = async (req: Request, res: Response): Promise<void> => {
+	const job = await Job.findById(req.params.id);
+
+	res.status(StatusCodes.OK).json({ job });
+}
+
+// ================== UPDATE JOB ==================
+export const updateJob = async (req: Request, res: Response): Promise<void> => {
+
+	const updatedJob = await Job.findByIdAndUpdate(req.params.id, req.body, {
+		// give you the object after update was applied.
+		new: true,
+	});
+	res.status(StatusCodes.OK).json({ msg: "Job modified!", job: updatedJob });
+}
+
+// ================== DELETE JOB ==================
+export const deleteJob = async (req: Request, res: Response): Promise<void> => {
+
+	const deletedJob = await Job.findByIdAndDelete(req.params.id);
+
+	res.status(StatusCodes.OK).json({ msg: "job deleted", job: deletedJob });
+}
--- server/routes/jobRouter.ts
+++ server/routes/jobRouter.ts
@@ -1,9 +1,15 @@
-import { createJob, getAllJobs } from "./../controllers/jobController.js";
+import { createJob, deleteJob, getAllJobs, getSingleJob, updateJob } from "./../controllers/jobController.js";
 import { Router } from "express";
 import express from "express";
 
 const router: Router = express.Router();
 
 router.route("/").get(getAllJobs).post(createJob);
+
+router
+	.route("/:id")
+    .get(getSingleJob)
+    .patch(updateJob)
+    .delete(deleteJob)
 
 export default router;
