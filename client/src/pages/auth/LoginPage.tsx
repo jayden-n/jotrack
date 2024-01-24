b1d5df5844e5486a3dd67150e539e6fbe89bc995
louis7andrada <144291325+louis7andrada@users.noreply.github.com>
2024-01-23 19:11:33 -0500

    Update LoginPage.tsx

--- client/src/pages/auth/LoginPage.tsx
+++ client/src/pages/auth/LoginPage.tsx
@@ -1,5 +1,7 @@
 import React, { useState, FormEvent, ChangeEvent } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
+
 
 // Define a type for the user role
 type UserRole = 'admin' | 'user' | null;
@@ -51,31 +53,37 @@ const LoginPage = () => {
 
 	return (
 		<div className="grid h-screen grid-cols-1 md:grid-cols-4 bg-e6e0e0">
-			<div className="flex flex-col justify-center md:col-span-1 bg-white p-10">
-				<div>
-					<div className="flex justify-center items-center">
-						<button className="text-5xl font-extrabold text-purple mb-14" onClick={() => navigate('/')}>JoTrack</button>
+			<div className="flex flex-col justify-center md:col-span-1 border border-gray-300 bg-white p-10">
+				<div className="mb-52">
+					<div className="flex justify-center items-center mb-14">
+						<ul className="text-purple text-lg ">
+							<li className="font-outfit text-3xl font-extrabold pr-10">
+								<Link to={'/'}>
+									<span className="text-5xl ml-10">J</span>oTrack
+								</Link>
+							</li>
+						</ul>
 					</div>
-					<hr className="border-t border-gray-300 mb-14" />
+					<hr className="border-t border-gray-300 mb-14 mx-auto my-4 w-3/4 h-0.5" />
 					<h2 className="text-3xl font-semibold text-center mb-32">Sign in</h2>
-					<p className="text-1xl text-gray text-center opacity-60 mb-8">Don't Have An Account?</p>
+					<p className="text-2xl text-gray text-center opacity-60 mb-8">Don't Have An Account?</p>
 					<div className="flex justify-center items-center">
-						<button className="text-2xl text-white bg-btnPurple py-3 px-24" onClick={() => navigate('/register')}>Sign up</button>
+						<button className="text-2xl text-white bg-btnPurple py-3 px-28" onClick={() => navigate('/register')}>Sign up</button>
 					</div>
 				</div>
 			</div>
 
 			<div className="grid place-items-center p-10 md:col-span-3">
-				<form onSubmit={handleSubmit} className="grid grid-cols-1 mt-20 gap-8 md:max-w-1xl lg:max-w-3xl mx-auto">
-					<h2 className="text-3xl font-semibold text-center mb-8 justify-self-center md:col-span-2">Log into your account</h2>
-					<input type="email" value={email} placeholder="email" className="p-4 text-2xl bg-white border-none rounded shadow-inne w-full justify-self-center mt-4 md:col-span-2" onChange={handleEmailChange} />
-					<input type="password" value={password} placeholder="password" className="p-4 text-2xl bg-white border-none rounded shadow-inner w-full justify-self-center mb-6 mt-4 md:col-span-2" onChange={handlePasswordChange} />
-					<hr className="border-t border-gray-300 my-8 mb-4 mx-auto w-1/2 justify-self-center mt-4 md:col-span-2" />
-					<div className="flex justify-center gap-4 md:col-span-2">
+				<form onSubmit={handleSubmit} className="grid grid-cols-1 gap-12 md:max-w-3xl lg:max-w-5xl mx-auto">
+					<h2 className="text-3xl font-semibold text-center mb-4 justify-self-center md:col-span-2">Log into your account</h2>
+					<input type="email" value={email} placeholder="email" className="p-4 text-2xl bg-white border border-gray-300 rounded shadow-inne w-full justify-self-center mt-4 md:col-span-2" onChange={handleEmailChange} />
+					<input type="password" value={password} placeholder="password" className="p-4 text-2xl bg-white border border-gray-300 rounded shadow-inner w-full justify-self-center mb-6 mt-4 md:col-span-2" onChange={handlePasswordChange} />
+					<hr className="border-t border-gray-300 my-8 mb-4 mx-auto w-1/3 justify-self-center mt-4 md:col-span-2" />
+					<div className="flex justify-center gap-12 md:col-span-2">
 					<button type="button" className={`w-36 py-3 text-2xl bg-white font-regular rounded-xl ${role === 'admin' ? 'bg-gray' : 'bg-transparent'}`} onClick={handleAdminClick}>Admin</button>
 					<button type="button" className={`w-36 py-3 text-2xl bg-white font-regular rounded-xl ${role === 'user' ? 'bg-gray' : 'bg-transparent'}`} onClick={handleUserClick}>User</button>
 					</div>
-					<button type="submit" className="text-2xl text-white bg-btnPurple py-3 px-52 justify-self-center mt-8 md:col-span-2">Sign in</button>
+					<button type="submit" className="text-2xl text-white bg-btnPurple py-3 px-72 justify-self-center mt-10 md:col-span-2">Sign in</button>
 					{error && <div className="text-red-500 py-2 px-6 justify-self-center md:col-span-2">{error}</div>}
 			</form>
 			</div>
