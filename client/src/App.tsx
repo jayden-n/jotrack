f572d8e25429762d3cdef4663c6fb4b5831f3e86
sarahrami <sarahrami97@gmail.com>
2024-01-28 00:20:10 -0500

    resolving merge

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -18,6 +18,7 @@ import UserViewResume from './pages/user/UserViewResume';
 import UserUpdatePasswordPage from './pages/user/UserUpdatePasswordPage';
 import UserAllJobsPage from './pages/user/UserAllJobsPage';
 import EditJob from './pages/admin/EditJob';
+import UserSingleJobPage from './pages/user/UserSingleJobPage';
 const router = createBrowserRouter([
 	{
 		path: '/',
