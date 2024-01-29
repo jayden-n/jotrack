964c0a5dcbb5b6f42ce0db02121fa437f1b4b9e5
Jerome Delos-Reyes <delosreyes.jerome@proton.me>
2024-01-29 15:15:36 -0500

    dockerized whole app but no connection between server and client yet; able to retrieve user activity as admin

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,16 +1,21 @@
-import { defineConfig } from 'vite';
+import {defineConfig} from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react()],
-	server: {
-		proxy: {
-			'/api': {
-				target: 'http://localhost:8000/api',
-				changeOrigin: true,
-				rewrite: (path) => path.replace(/^\/api/, ''),
-			},
-		},
-	},
+    plugins: [react()],
+    server: {
+        proxy: {
+            '/api': {
+                target: 'http://localhost:8000/api',
+                changeOrigin: true,
+                rewrite: (path) => path.replace(/^\/api/, ''),
+            },
+        },
+        host: true,
+        port: 5173,
+        watch: {
+            usePolling: true
+        }
+    },
 });
